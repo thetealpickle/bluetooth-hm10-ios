eafc00ab89d5558d8e12f66ecb9ffa06bfb5f289
Jessica Joseph <jessjeanjo@gmail.com>
2018-04-19 17:12:47 -0400

    discovering peripherals is working

--- bhm10/ViewController.swift
+++ bhm10/ViewController.swift
@@ -5,9 +5,12 @@ import CoreBluetooth
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     var items = [String: [String:Any]]()
+    
+    var devices: Dictionary<String, CBPeripheral> = [:]
+    var activeCentralManager: CBCentralManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +19,15 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
+        activeCentralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -32,25 +36,42 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return devices.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "bluetoothCell", for: indexPath) as! BluetoothCell
-        cell.signalLabel.text = "Sample"
+        
+        let discoveredPeripherals = Array(devices.values)
+        
+            if let name = discoveredPeripherals[indexPath.row].name {
+                cell.signalLabel.text = name
+            }
         
         cell.layer.cornerRadius = 15
-        return cell
+            return cell
     }
 }
 
 extension ViewController: CBCentralManagerDelegate, CBPeripheralDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == .poweredOn {
-            central.scanForPeripherals(withServices: nil, options: nil)
+            if central.state == .poweredOn {
+                central.scanForPeripherals(withServices: nil, options: nil)
+                print("Searching for BLE Devices")
+            } else {
+                print("Bluetooth switched off")
         }
     }
-
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if let name = peripheral.name {
+            if (devices[name] == nil) {
+                devices[name] = peripheral
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         peripheral.readRSSI()
     }
